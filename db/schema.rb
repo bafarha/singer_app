6db60f76ce272763b74f766e2c4ffab5d5180f06
Blake Farha <bafarha@gmail.com>
2015-03-25 22:13:23 +0100

    I created a model for Orders aand associated my orders to products

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150323223743) do
+ActiveRecord::Schema.define(version: 20150325205641) do
+
+  create_table "orders", force: :cascade do |t|
+    t.string "user_id"
+  end
 
   create_table "products", force: :cascade do |t|
     t.string   "name"

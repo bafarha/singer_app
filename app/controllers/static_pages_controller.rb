20c44fa0d2103fb8a0949fa88ed26e1d3b105b13
Blake Farha <bafarha@gmail.com>
2015-05-13 18:28:20 +0200

    added an email form on contact page.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -6,6 +6,13 @@ class StaticPagesController < ApplicationController
 		@products = Product.limit(4)
   end
 
+ActionMailer::Base.mail(:from => 'from@domain.com', :to=> 'to@domain.com', :subject => "Mr. T's awesome contacts", :body => 'I pity da fool.  Seriously.').deliver
+
+def thank_you
+	@name = params[:name]
+	@email = params[:email]
+	@message = params[:message]
+	ActionMailer::Base.mail(:from => @email, :to => 'bafarha@gmail.com', :subject => '#{@name} sent you a message, sucka!', :body => @message).deliver
 end
 
-
+end

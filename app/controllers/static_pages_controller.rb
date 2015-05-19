52edac4d40d74912b9878570090d393a918776cf
Blake Farha <bafarha@gmail.com>
2015-05-19 17:35:47 +0200

    added custom mailer

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -10,7 +10,14 @@ def thank_you
 	@name = params[:name]
 	@email = params[:email]
 	@message = params[:message]
-	ActionMailer::Base.mail(:from => @email, :to => 'bafarha@gmail.com', :subject => '#{@name} sent you a message, sucka!', :body => @message).deliver
+	ActionMailer::Base.mail(:from => @email, :to => 'bafarha@gmail.com', :subject => ' #@name sent you a message, sucka!', :body => @message).deliver
+end
+
+def thank_you
+  @name = params[:name]
+  @email = params[:email]
+  @message = params[:message]
+  UserMailer.contact_form(@email, @name, @message).deliver
 end
 
 end

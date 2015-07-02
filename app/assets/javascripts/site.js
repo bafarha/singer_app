8d151d24e5393b975b6f9c028d01ef6828e4e26f
Blake Farha <bafarha@gmail.com>
2015-07-03 01:42:18 +0200

    fixed the .rated class, so now stars show up in review.

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,9 +1,7 @@
 $(document).on('ready page:load', function(){
-	$('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
-	$('.rated').raty({ path: '/assets', 
-	  readOnly: true,
-	  score: function() {
-	    return $(this).attr('data-score');
-	  }
-	});
+	$('.rating').raty({ path: '/assets', scoreName: 'comment[rating]' });
+	$('.rated').raty( { path: '/assets', readOnly: true, score: function(){
+		return $(this).attr('data-score');
+		}
+	})
 });

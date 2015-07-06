8ccaee4a45cde49a24f7087e40686031aaad0c5c
Blake Farha <bafarha@gmail.com>
2015-07-07 00:28:48 +0200

    added validation logic to comments controller.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -6,4 +6,19 @@ class Comment < ActiveRecord::Base
   validates :product, presence: true
   validates :rating, numerically: { only_integer: true }
 
+  def create
+  	@product = Product.find(params[:product_id])
+  	@comment = @product.comments.new(comment_params)
+  	@comment.user = current_user
+  	respond_to do |format|
+  		if @comment.save
+  			format.html { redirect_to @product, notice: 'Thanks for sharing your thoughts!' }
+  			format.json { render :show, status: :created, location: @product }
+  		else
+  			format.html { redirect_to @product, alert: 'Your review was missing something...' }
+  			format.json { render json: @comment.errors, status: :unprocessabel_entity }
+  		end
+  	end
+  end
+  
 end

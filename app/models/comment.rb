adc3c9169fb5ce8df1098b4d6b81727551543352
Blake Farha <bafarha@gmail.com>
2015-07-07 00:20:58 +0200

    added validation to the comments model.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,9 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :product
+  validates :body, presence: true
+  validates :user, presence: true
+  validates :product, presence: true
+  validates :rating, numerically: { only_integer: true }
+
 end

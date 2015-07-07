ff1a2ad50e7ef7ef90adbd25c6cb196c2958fc83
Blake Farha <bafarha@gmail.com>
2015-07-07 19:02:29 +0200

    ran admin flag migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,23 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20150619215957) do
-=======
-ActiveRecord::Schema.define(version: 20150626005729) do
+ActiveRecord::Schema.define(version: 20150707165732) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "user_id"
-    t.text     "body"
-    t.integer  "rating"
     t.integer  "product_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "body"
+    t.integer  "rating"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
-
-  add_index "comments", ["product_id"], name: "index_comments_on_product_id"
-  add_index "comments", ["user_id"], name: "index_comments_on_user_id"
->>>>>>> before_things_broke
 
   create_table "orders", force: :cascade do |t|
     t.string  "user_id"
@@ -48,18 +41,19 @@ ActiveRecord::Schema.define(version: 20150626005729) do
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
     t.string   "last_name"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.datetime "created_at",                             null: false
+    t.datetime "updated_at",                             null: false
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.boolean  "admin",                  default: false, null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true

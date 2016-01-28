2ce156262c4cfcbfe4e38c7cabfe36000b8eb71d
Blake Farha <bafarha@gmail.com>
2016-01-29 00:03:44 +0100

    added stripe apis

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,2 +1,13 @@
 if Rails.env.production?
-	Rails
+	Rails.configuration.stripe = {
+		:publishable_key => ENV['STRIPE_PUBLISHABLE_KEY'],
+		:secret_key => ENV['STRIPE_SECRET_KEY']
+	}
+else
+	Rails.configuration.stripe = {
+		:publishable_key => 'pk_test_CQbixLZkV7LEBnpgNF846j2z Roll Key',
+		:secret_key => 'sk_test_gBnuiqIVWGLjwwZJsmZHm8IQ',
+	}
+end
+
+Stripe.api_key = Rails.configuration.stripe[:secret_key]

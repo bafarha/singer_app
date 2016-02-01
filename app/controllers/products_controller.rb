966df3fd3bc620a5c562bc927db2d588d52663a0
Blake Farha <bafarha@gmail.com>
2016-02-01 13:20:40 +0100

    added price column to products table, added @product.price to stripe form

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -81,7 +81,7 @@ class ProductsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
-      params.require(:product).permit(:name, :description, :image_url, :year)
+      params.require(:product).permit(:name, :description, :image_url, :year, :price)
     end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150707165732) do
+ActiveRecord::Schema.define(version: 20160201112557) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "user_id"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20150707165732) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "year"
+    t.decimal  "price"
   end
 
   create_table "users", force: :cascade do |t|

ea8fe151a40fe24a1703b4ff4aa06ca4e67f393c
Blake Farha <bafarha@gmail.com>
2016-02-07 01:24:39 +0100

    added elevateZoom

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,7 +1,17 @@
+/* start of raty */
+
 $(document).on('ready page:load', function(){
 	$('.rating').raty({ path: '/assets', scoreName: 'comment[rating]' });
 	$('.rated').raty( { path: '/assets', readOnly: true, score: function(){
 		return $(this).attr('data-score');
 		}
 	});
+	/* end of raty */
+
+	/* start of elevateZoom */
+	$('.img-zoom').elevateZoom();
+	/* end of eleveateZoom */
 });
+
+
+

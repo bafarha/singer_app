fcd6e086ea7c6278ae371cd87a4b8526fed33bca
Blake Farha <bafarha@gmail.com>
2016-03-16 10:58:01 +0100

    added return objects to app.js

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -15,15 +15,20 @@ app.factory('models', ['$resource', function($resource){
 }]);
 
 app.controller('OrdersCtrl', ['$scope', 'models', function($scope, models){
-	$scope.orders = models.orders;
+	$scope.orders = models.orders.query();
+	$scope.products = models.products.query();
 
 	$scope.addOrder = function(){
 		if(!$scope.newOrder.product_id || $scope.newOrder.total === ''){return; }
-		$scope.orders.push($scope.newOrder);
-		$scope.newOrder = '';
+		order = models.orders.save($scope.newOrder, function(){
+			recent_order = models.orders.get({id: order.id});
+			$scope.orders.push(recent_order);
+			$scope.newOrder = '';
+		});
 	}
 
 	$scope.deleteOrder = function(order){
+		models.orders.delete(order);
 		$scope.orders.splice($scope.orders.indexOf(order), 1);
 	}
 }]);
